18e8ce0714c9bce15f3e88f818ad27aa17e141de
quangbmk66dsai02 <128770333+quangbmk66dsai02@users.noreply.github.com>
2025-02-13 10:19:11 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,3 +16,29 @@ from unsloth import is_bfloat16_supported
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 data = pd.read_json("hf://datasets/Amod/mental_health_counseling_conversations/combined_dataset.json", lines=True)
 
+data['Context_length'] = data['Context'].apply(len)
+filtered_data = data[data['Context_length'] <= 1500]
+ln_Context = filtered_data['Context'].apply(len)
+ln_Response = filtered_data['Response'].apply(len)
+filtered_data = filtered_data[ln_Response <= 4000]
+ln_Response = filtered_data['Response'].apply(len)
+max_seq_length = 5020
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Llama-3.2-1B-bnb-4bit",
+    max_seq_length=max_seq_length,
+    load_in_4bit=True,
+    dtype=None,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    lora_alpha=16,
+    lora_dropout=0,
+    target_modules=["q_proj", "k_proj", "v_proj", "up_proj", "down_proj", "o_proj", "gate_proj"],
+    use_rslora=True,
+    use_gradient_checkpointing="unsloth",
+    random_state = 32,
+    loftq_config = None,
+)
+print(model.print_trainable_parameters())
