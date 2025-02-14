3ba85edfea5d49271cf38b48f83c4208273d8c24
quangbmk66dsai02 <128770333+quangbmk66dsai02@users.noreply.github.com>
2025-02-14 14:10:55 +0700

    Create test_infer.py

---
+++ test_infer.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+import torch
+from trl import SFTTrainer
+from transformers import TrainingArguments, TextStreamer
+from unsloth.chat_templates import get_chat_template
+from unsloth import FastLanguageModel
+from datasets import Dataset
+from unsloth import is_bfloat16_supported
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+
+text="I'm going through some things with my feelings and myself. I barely sleep and I do nothing but think about how I'm worthless and how I shouldn't be here. I've never tried or contemplated suicide. I've always wanted to fix my issues, but I never get around to it. How can I change my feeling of being worthless to everyone?"
+data_prompt = """Analyze the provided text from a mental health perspective. Identify any indicators of emotional distress, coping mechanisms, or psychological well-being. Highlight any potential concerns or positive aspects related to mental health, and provide a brief explanation for each observation.
+
+### Input:
+{}
+
+### Response:
+{}"""
+max_seq_length = 5020
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Llama-3.2-1B-bnb-4bit",
+    max_seq_length=max_seq_length,
+    load_in_4bit=True,
+    dtype=None,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    lora_alpha=16,
+    lora_dropout=0,
+    target_modules=["q_proj", "k_proj", "v_proj", "up_proj", "down_proj", "o_proj", "gate_proj"],
+    use_rslora=True,
+    use_gradient_checkpointing="unsloth",
+    random_state = 32,
+    loftq_config = None,
+)
+print(model.print_trainable_parameters())
+
+
+model = FastLanguageModel.for_inference(model)
+inputs = tokenizer(
+[
+    data_prompt.format(
+        #instructions
+        text,
+        #answer
+        "",
+    )
+], return_tensors = "pt").to("cuda")
+
+outputs = model.generate(**inputs, max_new_tokens = 5020, use_cache = True)
+answer= tokenizer.batch_decode(outputs)
+answer = answer[0].split("### Response:")[-1]
+print("Answer of the question is:", answer)
